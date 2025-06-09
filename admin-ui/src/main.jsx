6f2ade4eab001aa2120be5a0f70552b8168352e2
MCD <120051609+mcdnew@users.noreply.github.com>
2025-06-09 16:06:16 +0000

    Import Bootstrap CSS

--- admin-ui/src/main.jsx
+++ admin-ui/src/main.jsx
@@ -1,8 +1,17 @@
+// src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+// 1) Import Bootstrap’s CSS first
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 2) Then your own index.css (if you have custom styles)
 import './index.css'
 
+import App from './App'
+
+// This mounts your React app into the <div id="root"> in index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
